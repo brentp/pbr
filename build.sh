cf0a148fb976bf9f1d65c93b3f36197949fd5895
Brent Pedersen <bpederse@gmail.com>
2023-06-12 09:19:00 -0700

    add test bam

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 set -eu
 
-target=x86_64-unknown-linux-gnu
-export RUSTFLAGS="-C target-feature=-crt-static -C relocation-model=pic"
-cargo test --release --target $target && cargo build --release --target $target
-ls -lh ./target/$target/release/pbr
+#target=x86_64-unknown-linux-gnu
+#export RUSTFLAGS="-C target-feature=-crt-static -C relocation-model=pic"
+#cargo test --release --target $target && cargo build --release --target $target
+#ls -lh ./target/$target/release/pbr
+#exit
 
-#target=x86_64-unknown-linux-musl
-#cargo clean 
-#cross build --target=$target --release
-#ls -lhd ./target/$target/release/pbr
+target=x86_64-unknown-linux-musl
+cargo clean 
+cross build --target=$target --release
+ls -lhd ./target/$target/release/pbr
