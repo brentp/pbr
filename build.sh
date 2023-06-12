2a1630c15379adae117a80f049961b75633988f8
Brent Pedersen <bpederse@gmail.com>
2023-06-12 10:47:15 -0700

    fix build somehow

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 set -eu
 
 #target=x86_64-unknown-linux-gnu
-#export RUSTFLAGS="-C target-feature=-crt-static -C relocation-model=pic"
-#cargo test --release --target $target && cargo build --release --target $target
+#export RUSTFLAGS="-C target-feature=-crt-static -C relocation-model=pie"
+#cargo test --release --target $target 
+#RUSTFLAGS="-C target-feature=-crt-static -C relocation-model=pie" cargo build --release --target $target
 #ls -lh ./target/$target/release/pbr
 #exit
 
